ab7bac26a2079188c2779a48813bef5e7a81e118
Tufazzul1 <tufajjul2017@gmail.com>
2024-04-26 12:21:12 +0545

    create basic setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome Home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Login here or join with us</h2>
+        </div>
+    );
+};
+
+export default Login;
